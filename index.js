96893fdb23be74a82d883780345d8a8dd8b3c338
justinZ45 <justinzaluk@gmail.com>
2023-05-12 16:03:16 -0400

    cors use change

--- index.js
+++ index.js
@@ -9,11 +9,28 @@ const db = require("./db");
 const PORT = process.env.PORT || 5432;
 
 app.listen(PORT, () => {
-  console.log("server from port 5432");  //server running on port 5000
+  console.log("server from port 5432"); //server running on port 5000
 });
 
 //middleware
-
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // handle OPTIONS method
+  if ("OPTIONS" == req.method) {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(express.json()); //req.body
 app.use(compression());
 app.use(bodyParser.json());
